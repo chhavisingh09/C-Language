48cbe38e05730fc42a790104a16b007c29db3a01
Chhavi Singh <chhavisingh.0009@gmail.com>
2025-12-02 23:23:07 +0530

    Create input.c

---
+++ input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("\nYou entered: %d and %d\n", a, b);
+
+    return 0;
+}
