3f38f14fd265d2b9105bd78c6d578bf136a6a249
Chhavi Singh <chhavisingh.0009@gmail.com>
2025-12-02 23:19:07 +0530

    Create datatype.c

---
+++ datatype.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 18;
+    float marks = 92.5;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Marks: %f\n", marks);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
